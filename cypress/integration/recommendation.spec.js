19941b377fcb086922f42a015b97b79789b2ed81
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2022-05-06 14:49:54 -0300

    test: adds votes tests

--- cypress/integration/recommendation.spec.js
+++ cypress/integration/recommendation.spec.js
@@ -1,4 +1,4 @@
-beforeEach(() => {
+before(() => {
   cy.request("POST", "http://localhost:5000/tests/reset-database");
 });
 
@@ -10,12 +10,27 @@ describe("Recommendations E2E tests", () => {
     cy.get('input[placeholder*="https://youtu.be/..."]').type(
       "https://www.youtube.com/watch?v=yAoLSRbwxL8"
     );
-
     cy.intercept("POST", "http://localhost:5000/recommendations").as(
       "createRecommendation"
     );
     cy.get("div button").click();
     cy.wait("@createRecommendation");
     cy.contains("Dummy").should("be.visible");
+  });
+  it("should upvote successfully a recommendation", () => {
+    cy.intercept("POST", "http://localhost:5000/recommendations/*/upvote").as(
+      "upvote"
+    );
+    cy.get("svg[id*='upvote']").click();
+    cy.wait("@upvote");
+    cy.contains("1").should("be.visible");
+  });
+  it("should downvote successfully a recommendation", () => {
+    cy.intercept("POST", "http://localhost:5000/recommendations/*/downvote").as(
+      "downvote"
+    );
+    cy.get("svg[id*='downvote']").click();
+    cy.wait("@downvote");
+    cy.contains("0").should("be.visible");
   });
 });
