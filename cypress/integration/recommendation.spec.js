caa6dee642fefc9fa681c99e3325b91601f5a7bd
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2022-05-09 13:14:52 -0300

    basic requirements done

--- cypress/integration/recommendation.spec.js
+++ cypress/integration/recommendation.spec.js
@@ -1,5 +1,6 @@
 before(() => {
   cy.request("POST", "http://localhost:5000/tests/reset-database");
+  cy.request("POST", "http://localhost:5000/tests/seed/recommendations");
 });
 
 describe("Recommendations E2E tests", () => {
@@ -21,7 +22,7 @@ describe("Recommendations E2E tests", () => {
     cy.intercept("POST", "http://localhost:5000/recommendations/*/upvote").as(
       "upvote"
     );
-    cy.get("svg[id*='upvote']").click();
+    cy.get("svg[id*='upvote']").first().click();
     cy.wait("@upvote");
     cy.contains("1").should("be.visible");
   });
@@ -29,8 +30,31 @@ describe("Recommendations E2E tests", () => {
     cy.intercept("POST", "http://localhost:5000/recommendations/*/downvote").as(
       "downvote"
     );
-    cy.get("svg[id*='downvote']").click();
+    cy.get("svg[id*='downvote']").first().click();
     cy.wait("@downvote");
     cy.contains("0").should("be.visible");
+  });
+  it("should get 10 recommendations", () => {
+    cy.get(".sc-gKXOVf")
+      .find("div")
+      .should("have.length", 11 * 4);
+  });
+  it("should get top recommendations", () => {
+    cy.get("div").contains("Top").first().click();
+    cy.location().should((loc) => {
+      expect(loc.toString()).to.eq("http://localhost:3000/top");
+    });
+    cy.get(".sc-gKXOVf")
+      .find("div")
+      .should("have.length", 10 * 4);
+  });
+  it("should get a random recommendation", () => {
+    cy.get("div").contains("Random").first().click();
+    cy.location().should((loc) => {
+      expect(loc.toString()).to.eq("http://localhost:3000/random");
+    });
+    cy.get(".sc-gKXOVf")
+      .find("div")
+      .should("have.length", 1 * 4);
   });
 });
